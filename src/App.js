66340f316683eebe6a963d77ea273eff693b9c79
Sam <sam.eskandar00@hotmail.com>
2020-06-03 12:17:42 -0400

    add redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import Post from './Components/Post';
 import PostForm from "./Components/PostForm";
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <PostForm/>
-                <hr style={{ marginRight: '70px', marginLeft: '70px'}}/>
-                <Post/>
-            </div>
+            <Provider store={store}>
+                <div>
+                    <PostForm/>
+                    <hr style={{ marginRight: '70px', marginLeft: '70px'}}/>
+                    <Post/>
+                </div>
+            </Provider>
         );
     }
 }
