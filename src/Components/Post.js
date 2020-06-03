deaec8fd5e07fcb6cd9a90b100b8c2000f1561a7
Sam <sam.eskandar00@hotmail.com>
2020-06-03 11:20:48 -0400

    update style for posts display

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,16 +3,33 @@ import React from 'react';
 class Post extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            posts: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => this.setState({ posts: data }))
     }
 
     render() {
         return (
             <div>
-
+                <h1 style={{margin: '70px'}}>Posts</h1>
+                <div className="post-items">
+                    {this.state.posts.map(post =>
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                    )}
+                </div>
             </div>
         )
     }
 
 }
 
-export default Posts
+export default Post;
